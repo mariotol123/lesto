109d9e3d4bd6a54c2650b242de7c9a233d64286e
Mario <mariobros9@hotmail.it>
2025-08-08 22:44:57 +0200

    Modifiche codice

--- httpdocs/wp-content/themes/lesto-theme/template-home.php
+++ httpdocs/wp-content/themes/lesto-theme/template-home.php
@@ -13,46 +13,36 @@ get_header();
 	<!-- Main content area -->
 	<main>
 		<section class="section container">
-			<div class="content-section vh-100 d-flex flex-column justify-content-center">
-				<div class="row w-100 flex-grow-1 align-items-center">
-					<!-- Colonna testo -->
-					<div id="text-column" class="col-md-10 d-flex flex-column justify-content-center">
-						<!-- Contenuto per "Nuovo locale" -->
-						<div id="content-locale" class="content-box active tabcontent fade-content show text-start" data-tab="Tab2">
-							<h1 class="mb-4 w-100">Vuoi aprire<br>un locale?</h1>
-							<p class="page-description mb-4 w-50">Dal 1984 Lesto Group si occupa di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti, hotel, pizzerie e bar.</p>
-						</div>
-						
-						<!-- Contenuto per "Catena" -->
-						<div id="content-franchise" class="content-box tabcontent fade-content text-start" data-tab="Tab1">
-							<h1 class="mb-4 w-100">Vuoi aprire<br>un format replicabile?</h1>
-							<p class="page-description mb-4 w-50">Dal 1984 Lesto Group si occupa di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti, hotel, pizzerie e bar.
-								</p>
-								<p class="page-description w-50">Entra nel nostro showroom e scopri i nostri servizi.</p>
-						</div>
+			<div class="row align-items-center vh-100">
+				<div id="text-column" class="col-md-10 d-flex flex-column justify-content-center">
+					<div id="content-locale" class="content-box active tabcontent fade-content show text-start" data-tab="Tab2">
+						<h1 class="mb-4 w-100">Vuoi aprire<br>un locale?</h1>
+						<p class="page-description mb-4 w-50">Dal 1984 Lesto Group si occupa di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti, hotel, pizzerie e bar.</p>
 					</div>
-					
-					<!-- Colonna icone social -->
-					<div id="social-column" class="col-md-2 d-flex align-items-end justify-content-end position-relative">
-						<div class="social-icons-container">
-							<a href="#" class="social-link">
-								<svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect width="35" height="35" rx="17.5" fill="white" fill-opacity="0.01"/>
-									<path d="M17.5 30.625C19.2236 30.625 20.9303 30.2855 22.5227 29.6259C24.1151 28.9663 25.562 27.9995 26.7808 26.7808C27.9995 25.562 28.9663 24.1151 29.6259 22.5227C30.2855 20.9303 30.625 19.2236 30.625 17.5C30.625 15.7764 30.2855 14.0697 29.6259 12.4773C28.9663 10.8849 27.9995 9.43799 26.7808 8.21922C25.562 7.00045 24.1151 6.03367 22.5227 5.37408C20.9303 4.71449 19.2236 4.375 17.5 4.375C14.019 4.375 10.6806 5.75781 8.21922 8.21922C5.75781 10.6806 4.375 14.019 4.375 17.5C4.375 20.981 5.75781 24.3194 8.21922 26.7808C10.6806 29.2422 14.019 30.625 17.5 30.625ZM17.5 30.625V14.5833C17.5 13.8098 17.8073 13.0679 18.3543 12.5209C18.9013 11.974 19.6431 11.6667 20.4167 11.6667H21.875M21.1458 18.2292H13.8542" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-								</svg>
-							</a>
-							<a href="#" class="social-link">
-								<svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect width="35" height="35" rx="17.5" fill="white" fill-opacity="0.01"/>
-									<path d="M21.875 5.10419H13.125C8.69518 5.10419 5.10413 8.69524 5.10413 13.125V21.875C5.10413 26.3048 8.69518 29.8959 13.125 29.8959H21.875C26.3047 29.8959 29.8958 26.3048 29.8958 21.875V13.125C29.8958 8.69524 26.3047 5.10419 21.875 5.10419Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									<path d="M17.5 22.7588C20.4043 22.7588 22.7587 20.4043 22.7587 17.5C22.7587 14.5957 20.4043 12.2413 17.5 12.2413C14.5956 12.2413 12.2412 14.5957 12.2412 17.5C12.2412 20.4043 14.5956 22.7588 17.5 22.7588Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-									<path d="M24.6371 11.865C25.4667 11.865 26.1392 11.1925 26.1392 10.3629C26.1392 9.53334 25.4667 8.86084 24.6371 8.86084C23.8075 8.86084 23.135 9.53334 23.135 10.3629C23.135 11.1925 23.8075 11.865 24.6371 11.865Z" fill="white"/>
-								</svg>
-							</a>
-						</div>
+					<div id="content-franchise" class="content-box tabcontent fade-content text-start" data-tab="Tab1">
+						<h1 class="mb-4 w-100">Vuoi aprire<br>un format replicabile?</h1>
+						<p class="page-description mb-4 w-50">Dal 1984 Lesto Group si occupa di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti, hotel, pizzerie e bar.</p>
+						<p class="page-description w-50">Entra nel nostro showroom e scopri i nostri servizi.</p>
 					</div>
 				</div>
-				
+				<div id="social-column" class="col-md-2 d-flex align-items-center justify-content-end position-relative p-0">
+					<div class="social-icons-container">
+						<a href="#" class="social-link">
+							<svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="35" height="35" rx="17.5" fill="white" fill-opacity="0.01"/>
+								<path d="M17.5 30.625C19.2236 30.625 20.9303 30.2855 22.5227 29.6259C24.1151 28.9663 25.562 27.9995 26.7808 26.7808C27.9995 25.562 28.9663 24.1151 29.6259 22.5227C30.2855 20.9303 30.625 19.2236 30.625 17.5C30.625 15.7764 30.2855 14.0697 29.6259 12.4773C28.9663 10.8849 27.9995 9.43799 26.7808 8.21922C25.562 7.00045 24.1151 6.03367 22.5227 5.37408C20.9303 4.71449 19.2236 4.375 17.5 4.375C14.019 4.375 10.6806 5.75781 8.21922 8.21922C5.75781 10.6806 4.375 14.019 4.375 17.5C4.375 20.981 5.75781 24.3194 8.21922 26.7808C10.6806 29.2422 14.019 30.625 17.5 30.625ZM17.5 30.625V14.5833C17.5 13.8098 17.8073 13.0679 18.3543 12.5209C18.9013 11.974 19.6431 11.6667 20.4167 11.6667H21.875M21.1458 18.2292H13.8542" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+						</a>
+						<a href="#" class="social-link">
+							<svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<rect width="35" height="35" rx="17.5" fill="white" fill-opacity="0.01"/>
+								<path d="M21.875 5.10419H13.125C8.69518 5.10419 5.10413 8.69524 5.10413 13.125V21.875C5.10413 26.3048 8.69518 29.8959 13.125 29.8959H21.875C26.3047 29.8959 29.8958 26.3048 29.8958 21.875V13.125C29.8958 8.69524 26.3047 5.10419 21.875 5.10419Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M17.5 22.7588C20.4043 22.7588 22.7587 20.4043 22.7587 17.5C22.7587 14.5957 20.4043 12.2413 17.5 12.2413C14.5956 12.2413 12.2412 14.5957 12.2412 17.5C12.2412 20.4043 14.5956 22.7588 17.5 22.7588Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M24.6371 11.865C25.4667 11.865 26.1392 11.1925 26.1392 10.3629C26.1392 9.53334 25.4667 8.86084 24.6371 8.86084C23.8075 8.86084 23.135 9.53334 23.135 10.3629C23.135 11.1925 23.8075 11.865 24.6371 11.865Z" fill="white"/>
+							</svg>
+						</a>
+					</div>
+				</div>
 			</div>
 		</section>
 
@@ -179,7 +169,7 @@ get_header();
 				<div class="col-md-5 d-flex align-items-center">
 					<h4 class="mt-4 mb-0">Grazie all’esperienza maturata in oltre 30 anni di attività e ad uno staff competente e disponibile, siamo in grado di soddisfare anche le richieste più esigenti, offrendo soluzioni complete e di alto livello.</h4>
 				</div>
-				<div class="col-md-7 d-flex align-items-end justify-content-md-end justify-content-center">
+				<div class="col-md-7 d-flex align-items-end justify-content-md-end justify-content-center p-0">
 					<button type="button" class="btn btn-custom mt-3 mt-md-0">Scopri di più</button>
 				</div>
 			</div>
