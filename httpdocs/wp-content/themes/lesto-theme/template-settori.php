f9216f9cc49ef6c1ded7748a1ceb178311dc630f
Mario <mariobros9@hotmail.it>
2025-08-11 18:19:30 +0200

    Codice formattato

--- httpdocs/wp-content/themes/lesto-theme/template-settori.php
+++ httpdocs/wp-content/themes/lesto-theme/template-settori.php
@@ -10,29 +10,32 @@
 get_header();
 ?>
 
-	<!-- Main content area -->
-	<main class="main-settori">
-		<div class="settori-container">
-            <h2 class="container pt-7xl">Settori</h2>
-        </div>
-        <hr class="footer-divider border-2 opacity-75 w-100">
-        <div class="container py-3">
-            <div class="row">
-                <div class="col-md-4 d-flex flex-column align-items-start">
-                    <h5>dalla progettazione alla manutenzione, la tua cucina professionale.</h5>
-                    <p class="mt-3">La tua cucina, al livello di performance che si merita. Dal 1984 Lesto Group si occupa di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti, hotel, pizzerie, gastronomie e bar.</p>
-                </div>
-                <div class="col-md-8 d-flex justify-content-center align-items-center">
-                    <img src="/wp-content/themes/lesto-theme/images/img-settori.png" alt="Immagine Settori" class="img-fluid img-overflow" />
-                </div>
+<!-- Main content area -->
+<main class="main-settori">
+    <div class="settori-container">
+        <h2 class="container pt-7xl">Settori</h2>
+    </div>
+    <hr class="footer-divider border-2 opacity-75 w-100">
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-md-4 d-flex flex-column align-items-start">
+                <h5>dalla progettazione alla manutenzione, la tua cucina professionale.</h5>
+                <p class="mt-3">La tua cucina, al livello di performance che si merita. Dal 1984 Lesto Group si occupa
+                    di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti,
+                    hotel, pizzerie, gastronomie e bar.</p>
+            </div>
+            <div class="col-md-8 d-flex justify-content-center align-items-center">
+                <img src="/wp-content/themes/lesto-theme/images/img-settori.png" alt="Immagine Settori"
+                    class="img-fluid img-overflow" />
             </div>
         </div>
+    </div>
 
-        
-            <hr class="footer-divider border-2 opacity-75 w-100">
 
-        <div class="pt-8xl pb-8xl">
-            <div class="container accordion-container header-buttons d-flex flex-column align-items-start">
+    <hr class="footer-divider border-2 opacity-75 w-100">
+
+    <div class="pt-8xl pb-8xl">
+        <div class="container accordion-container header-buttons d-flex flex-column align-items-start">
             <div class="header-buttons">
                 <button class="accordion-button btn btn-header-custom">Cucine</button>
             </div>
@@ -46,10 +49,10 @@ get_header();
                 <button class="accordion-button btn btn-header-custom">Lestowatt</button>
             </div>
         </div>
-        </div>
+    </div>
 
-	   </main>
-        
+</main>
+
 <?php
 
 get_footer();
