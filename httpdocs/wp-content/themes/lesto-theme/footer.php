f5c653506fa935286d0f3d104c382d37af49e7b4
Mario <mariobros9@hotmail.it>
2025-08-12 08:52:08 +0200

    Footer aggiustato

--- httpdocs/wp-content/themes/lesto-theme/footer.php
+++ httpdocs/wp-content/themes/lesto-theme/footer.php
@@ -13,62 +13,63 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="container footer-container mt-4">
-			<div class="row">
-				<!-- Prima colonna: Logo -->
-				<div class="col-lg-3 mb-4">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/Livello_1.png" alt="Footer Logo" class="footer-logo">
-				</div>
-				
-				<!-- Seconda colonna: Navigation e Social -->
-				<div class="col-lg-3 col-md-6 mb-4 d-flex flex-column gap-5">
-					<div class="footer-navigation mb-3 d-flex flex-column gap-3">
-						<h4 class="footer-title">Navigation</h4>
-						<ul class="footer-nav-list">
-							<li>Home</a></li>
-							<li>Chi Siamo</li>
-							<li>Servizi</li>
-							<li>Portfolio</li>
-							<li>Blog</li>
-							<li>Contatti</li>
-						</ul>
-					</div>
-					
-					<div class="footer-social d-flex flex-column gap-3">
-						<h4 class="footer-title">Social</h4>
-						<ul class="footer-social-list">
-							<li>Facebook</a></li>
-							<li>Instagram</a></li>
-							<li>LinkedIn</a></li>
-							<li>Twitter</a></li>
-						</ul>
-					</div>
-				</div>
-				
-				<!-- Terza colonna: Informazioni Aziendali e Contatti -->
-				<div class="col-lg-6 col-md-6 mb-4 d-flex flex-column gap-5">
-					<div class="footer-company mb-3 d-flex flex-column gap-3">
-						<h4 class="footer-title">Informazioni aziendali</h4>
-						<ul class="footer-company-list">
-							<li>Lesto Srl</li>
-							<li>P.IVA: <a href="https://www.example.com/piva" target="_blank">12345678901</a></li>
-							<li>Cod. Fisc.: <a href="https://www.example.com/codfisc" target="_blank">12345678901</a></li>
-							<li>REA: <a href="https://www.example.com/rea" target="_blank">MI-1234567</a></li>
-							<li><a href="<?php echo home_url('/privacy/'); ?>" class="text-uppercase">Privacy Policy</a></li>
-							<li><a href="<?php echo home_url('/terms/'); ?>" class="text-uppercase">Termini e Condizioni</a></li>
-						</ul>
-					</div>
-					
-					<div class="footer-contacts d-flex flex-column gap-3">
-						<h4 class="footer-title">Contatti</h4>
-						<ul class="footer-contacts-list">
-							<li>Progettazione e showroom: <a href="https://www.google.com/maps?q=Via+Roma+123,+Milano" target="_blank"> Via Roma 123, Milano</a></li>
-							<li>Telefono: <a href="tel:+390212345678">+39 02 1234 5678</a></li>
-							<li>Email: <a href="mailto:info@lesto.it">info@lesto.it</a></li>
-							<li>PEC: <a href="mailto:pec@lesto.it">pec@lesto.it</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
+		   <!-- Prima row: logo, navigation, azienda -->
+		   <div class="row">
+			   <div class="col-lg-3 mb-4">
+				   <img src="<?php echo get_template_directory_uri(); ?>/images/Livello_1.png" alt="Footer Logo" class="footer-logo">
+			   </div>
+			   <div class="col-lg-3 col-md-6 mb-4 d-flex flex-column gap-5">
+				   <div class="footer-navigation mb-3 d-flex flex-column gap-3">
+					   <h4 class="footer-title">Navigation</h4>
+					   <ul class="footer-nav-list">
+						   <li>Home</li>
+						   <li>Chi Siamo</li>
+						   <li>Servizi</li>
+						   <li>Portfolio</li>
+						   <li>Blog</li>
+						   <li>Contatti</li>
+					   </ul>
+				   </div>
+			   </div>
+			   <div class="col-lg-6 col-md-6 mb-4 d-flex flex-column gap-5">
+				   <div class="footer-company mb-3 d-flex flex-column gap-3">
+					   <h4 class="footer-title">Informazioni aziendali</h4>
+					   <ul class="footer-company-list">
+						   <li>Lesto Srl</li>
+						   <li>P.IVA: <a href="https://www.example.com/piva" target="_blank">12345678901</a></li>
+						   <li>Cod. Fisc.: <a href="https://www.example.com/codfisc" target="_blank">12345678901</a></li>
+						   <li>REA: <a href="https://www.example.com/rea" target="_blank">MI-1234567</a></li>
+						   <li><a href="<?php echo home_url('/privacy/'); ?>" class="text-uppercase">Privacy Policy</a></li>
+						   <li><a href="<?php echo home_url('/terms/'); ?>" class="text-uppercase">Termini e Condizioni</a></li>
+					   </ul>
+				   </div>
+			   </div>
+		   </div>
+		   <!-- Seconda row: social e contatti -->
+		   <div class="row">
+			   <div class="col-lg-3 col-md-6 mb-4 offset-lg-3 d-flex flex-column gap-5">
+				   <div class="footer-social d-flex flex-column gap-3">
+					   <h4 class="footer-title">Social</h4>
+					   <ul class="footer-social-list">
+						   <li>Facebook</li>
+						   <li>Instagram</li>
+						   <li>LinkedIn</li>
+						   <li>Twitter</li>
+					   </ul>
+				   </div>
+			   </div>
+			   <div class="col-lg-6 col-md-6 mb-4 d-flex flex-column gap-5">
+				   <div class="footer-contacts d-flex flex-column gap-3">
+					   <h4 class="footer-title">Contatti</h4>
+					   <ul class="footer-contacts-list">
+						   <li>Progettazione e showroom: <a href="https://www.google.com/maps?q=Via+Roma+123,+Milano" target="_blank"> Via Roma 123, Milano</a></li>
+						   <li>Telefono: <a href="tel:+390212345678">+39 02 1234 5678</a></li>
+						   <li>Email: <a href="mailto:info@lesto.it">info@lesto.it</a></li>
+						   <li>PEC: <a href="mailto:pec@lesto.it">pec@lesto.it</a></li>
+					   </ul>
+				   </div>
+			   </div>
+		   </div>
 			<!-- Divider fuori dal container solo su mobile -->
 			<hr class="footer-divider border-2 opacity-75 d-block d-md-none m-0 mt-5">
 			<!-- Divider dentro il container solo su desktop -->
