e29c0a2ff625d23c44dc2a79378bcf5185bafc6c
Mario <mariobros9@hotmail.it>
2025-08-13 15:48:06 +0200

    Indentato meglio

--- httpdocs/wp-content/themes/lesto-theme/template-settori.php
+++ httpdocs/wp-content/themes/lesto-theme/template-settori.php
@@ -20,7 +20,8 @@ get_header();
         <div class="row">
             <div class="col-md-4 d-flex flex-column align-items-start">
                 <h5>dalla progettazione alla manutenzione, la tua cucina professionale.</h5>
-                <p class="mt-3 desktop-p">La tua cucina, al livello di performance che si merita. Dal 1984 Lesto Group si occupa
+                <p class="mt-3 desktop-p">La tua cucina, al livello di performance che si merita. Dal 1984 Lesto Group
+                    si occupa
                     di consulenza, progettazione, vendita e assistenza di attrezzature professionali per ristoranti,
                     hotel, pizzerie, gastronomie e bar.</p>
             </div>
@@ -34,136 +35,148 @@ get_header();
 
     <hr class="footer-divider border-2 opacity-75 w-100">
 
-   <div class="pt-8xl pb-8xl container">
-    <div class="accordion-container buttons d-flex flex-column align-items-start">
+    <div class="pt-8xl pb-8xl container">
+        <div class="accordion-container buttons d-flex flex-column align-items-start">
 
-        <div class="buttons">
-            <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
-                <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
-                <span>Cucine</span>
-            </button>
-            <div class="accordion-content" style="display:none;">
-                <h3 class="mb-3">Cucine aziendali</h3>
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali" class="img-fluid" />
-                    </div>
-                    <div class="col-md-6 d-flex flex-column align-items-start">
-                        <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
-                        <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a migliorare la ristorazione nella tua azienda.</p>
-                        <button class="btn btn-header-custom">Contattaci</button>
+            <div class="buttons">
+                <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
+                    <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
+                    <span>Cucine</span>
+                </button>
+                <div class="accordion-content" style="display:none;">
+                    <h3 class="mb-3">Cucine aziendali</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali"
+                                class="img-fluid" />
+                        </div>
+                        <div class="col-md-6 d-flex flex-column align-items-start">
+                            <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
+                            <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali
+                                e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a
+                                migliorare la ristorazione nella tua azienda.</p>
+                            <button class="btn btn-header-custom">Contattaci</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="buttons">
-            <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
-                <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
-                <span>Arrebo bar</span>
-            </button>
-            <div class="accordion-content" style="display:none;">
-                <h3 class="mb-3">Cucine aziendali</h3>
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali" class="img-fluid" />
-                    </div>
-                    <div class="col-md-6 d-flex flex-column align-items-start">
-                        <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
-                        <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a migliorare la ristorazione nella tua azienda.</p>
-                        <button class="btn btn-header-custom">Contattaci</button>
+            <div class="buttons">
+                <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
+                    <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
+                    <span>Arrebo bar</span>
+                </button>
+                <div class="accordion-content" style="display:none;">
+                    <h3 class="mb-3">Cucine aziendali</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali"
+                                class="img-fluid" />
+                        </div>
+                        <div class="col-md-6 d-flex flex-column align-items-start">
+                            <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
+                            <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali
+                                e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a
+                                migliorare la ristorazione nella tua azienda.</p>
+                            <button class="btn btn-header-custom">Contattaci</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="buttons">
-            <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
-                <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
-                <span>Cucine aziendali</span>
-            </button>
-            <div class="accordion-content" style="display:none;">
-                <h3 class="mb-3">Cucine aziendali</h3>
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali" class="img-fluid" />
-                    </div>
-                    <div class="col-md-6 d-flex flex-column align-items-start">
-                        <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
-                        <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a migliorare la ristorazione nella tua azienda.</p>
-                        <button class="btn btn-header-custom">Contattaci</button>
+            <div class="buttons">
+                <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
+                    <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
+                    <span>Cucine aziendali</span>
+                </button>
+                <div class="accordion-content" style="display:none;">
+                    <h3 class="mb-3">Cucine aziendali</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali"
+                                class="img-fluid" />
+                        </div>
+                        <div class="col-md-6 d-flex flex-column align-items-start">
+                            <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
+                            <p class="mb-3 desktop-p">Progettiamo e realizziamo cucine aziendali efficienti, funzionali
+                                e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a
+                                migliorare la ristorazione nella tua azienda.</p>
+                            <button class="btn btn-header-custom">Contattaci</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="buttons">
-            <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
-                <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
-                <span>Lestowatt</span>
-            </button>
-            <div class="accordion-content" style="display:none;">
-                <h3 class="mb-3">Cucine aziendali</h3>
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali" class="img-fluid" />
-                    </div>
-                    <div class="col-md-6 d-flex flex-column align-items-start">
-                        <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
-                        <p class="mb-3">Progettiamo e realizziamo cucine aziendali efficienti, funzionali e personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a migliorare la ristorazione nella tua azienda.</p>
-                        <button class=" btn btn-header-custom">Contattaci</button>
+            <div class="buttons">
+                <button class="accordion-button btn btn-header-custom d-flex align-items-center" type="button">
+                    <img class="icon" src="/wp-content/themes/lesto-theme/images/Group 1.png" alt="icon" />
+                    <span>Lestowatt</span>
+                </button>
+                <div class="accordion-content" style="display:none;">
+                    <h3 class="mb-3">Cucine aziendali</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/wp-content/themes/lesto-theme/images/Rectangle 1.png" alt="Cucine aziendali"
+                                class="img-fluid" />
+                        </div>
+                        <div class="col-md-6 d-flex flex-column align-items-start">
+                            <h5 class="mb-2 m_h5">Soluzioni su misura per la tua azienda</h5>
+                            <p class="mb-3">Progettiamo e realizziamo cucine aziendali efficienti, funzionali e
+                                personalizzate per ogni esigenza produttiva. Scopri come possiamo aiutarti a migliorare
+                                la ristorazione nella tua azienda.</p>
+                            <button class=" btn btn-header-custom">Contattaci</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    </div>
-</div>
-<div class="preventivo-video-wrapper">
-    <video class="preventivo-bg-video" autoplay loop muted playsinline>
-    <source src="/wp-content/themes/lesto-theme/videos/6613032-hd_1920_1080_25fps (1).mp4" type="video/mp4">
-    Il tuo browser non supporta il video di sfondo.
-    </video>
-    <div class="container preventivo-container py-5">
-        <div class="row">
-            <div class="col-md-6">
-                <h3>Contattaci per <br>un preventivo gratuito</h3>
-                <h4 class="mt-5">Ti risponderemo entro 24h</h4>
-            </div>
-            <div class="col-md-6 d-flex justify-content-end align-items-end">
-                <button class="btn btn-custom">Scopri di più</button>
-            </div>
         </div>
     </div>
-</div>
+    <div class="preventivo-video-wrapper">
+        <video class="preventivo-bg-video" autoplay loop muted playsinline>
+            <source src="/wp-content/themes/lesto-theme/videos/6613032-hd_1920_1080_25fps (1).mp4" type="video/mp4">
+            Il tuo browser non supporta il video di sfondo.
+        </video>
+        <div class="container preventivo-container py-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Contattaci per <br>un preventivo gratuito</h3>
+                    <h4 class="mt-5">Ti risponderemo entro 24h</h4>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end align-items-end">
+                    <button class="btn btn-custom">Scopri di più</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-</script>
+    </script>
 
-<script>
-document.querySelectorAll(".accordion-button").forEach(btn => {
-    btn.addEventListener("click", () => {
-        const content = btn.nextElementSibling;
-        const isVisible = content.style.display === "block";
-        // Chiude tutti
-        document.querySelectorAll(".accordion-content").forEach(c => c.style.display = "none");
-        // Rimuove la classe active da tutti i genitori e dai button
-        document.querySelectorAll(".accordion-button").forEach(b => {
-            if (b.parentElement) b.parentElement.classList.remove("active");
-            b.classList.remove("active");
+    <script>
+        document.querySelectorAll(".accordion-button").forEach(btn => {
+            btn.addEventListener("click", () => {
+                const content = btn.nextElementSibling;
+                const isVisible = content.style.display === "block";
+                // Chiude tutti
+                document.querySelectorAll(".accordion-content").forEach(c => c.style.display = "none");
+                // Rimuove la classe active da tutti i genitori e dai button
+                document.querySelectorAll(".accordion-button").forEach(b => {
+                    if (b.parentElement) b.parentElement.classList.remove("active");
+                    b.classList.remove("active");
+                });
+                // Apre solo quello cliccato
+                if (!isVisible) {
+                    content.style.display = "block";
+                    if (btn.parentElement) btn.parentElement.classList.add("active");
+                    btn.classList.add("active");
+                } else {
+                    if (btn.parentElement) btn.parentElement.classList.remove("active");
+                    btn.classList.remove("active");
+                }
+            });
         });
-        // Apre solo quello cliccato
-        if (!isVisible) {
-            content.style.display = "block";
-            if (btn.parentElement) btn.parentElement.classList.add("active");
-            btn.classList.add("active");
-        } else {
-            if (btn.parentElement) btn.parentElement.classList.remove("active");
-            btn.classList.remove("active");
-        }
-    });
-});
-</script>
+    </script>
 
 
 </main>
